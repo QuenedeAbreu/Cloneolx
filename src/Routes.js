0881c7beaf76ebb7d871334ba899176621195243
Quenede Abreu <quenede.in@gmail.com>
2021-08-25 22:54:04 -0500

    add sistem of login

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { Switch,Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
 import Sobre from "./pages/About";
+import NotFound from "./pages/NotFound"
 
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
-  <Switch>
+    <Switch>
       <Route exact path="/" >
         <Home />
       </Route>
-  
+
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+
       <Route path="/sobre" >
         <Sobre />
       </Route>
-  </Switch>
+
+
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
   );
 }
